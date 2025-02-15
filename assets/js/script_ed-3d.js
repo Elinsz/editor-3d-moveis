7a919033b27d36ab321f5ff2481c53d56e16b103
Elinsz <85796934+Elinsz@users.noreply.github.com>
2025-02-15 17:43:58 -0300

    cm

--- assets/js/script_ed-3d.js
+++ assets/js/script_ed-3d.js
@@ -92,7 +92,7 @@ function applyDimensions() {
 
     driverBlock.geometry.dispose();
     driverBlock.geometry = new THREE.BoxGeometry(width, height, depth);
-    driverBlock.position.set(0, height / 2, 0);
+    driverBlock.position.set(0, height / 2, width / 2);
 }
 
     window.onload = addEnvironment;
