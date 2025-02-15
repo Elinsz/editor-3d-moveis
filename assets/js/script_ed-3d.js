2ced46ac16051abda6a66d1dd66622724269da85
Elinsz <85796934+Elinsz@users.noreply.github.com>
2025-02-15 00:12:21 -0300

    cm

--- assets/js/script_ed-3d.js
+++ assets/js/script_ed-3d.js
@@ -30,9 +30,9 @@ document.addEventListener("DOMContentLoaded", () => {
     // Eixos X, Y, Z
     const axisLength = 300;
 
-    const xAxis = new THREE.ArrowHelper(new THREE.Vector3(1, 0, 0), new THREE.Vector3(0, 0, 0), axisLength, 0xff0000, 20, 10);
+    const xAxis = new THREE.ArrowHelper(new THREE.Vector3(0, 0, 1), new THREE.Vector3(0, 0, 0), axisLength, 0xff0000, 20, 10);
     const yAxis = new THREE.ArrowHelper(new THREE.Vector3(0, 1, 0), new THREE.Vector3(0, 0, 0), axisLength, 0x00ff00, 20, 10);
-    const zAxis = new THREE.ArrowHelper(new THREE.Vector3(0, 0, 1), new THREE.Vector3(0, 0, 0), axisLength, 0x0000ff, 20, 10);
+    const zAxis = new THREE.ArrowHelper(new THREE.Vector3(1, 0, 0), new THREE.Vector3(0, 0, 0), axisLength, 0x0000ff, 20, 10);
 
     scene.add(xAxis);
     scene.add(yAxis);
