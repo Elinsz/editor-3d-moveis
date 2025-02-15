190b1ac851f6930afa4718e51d103be06d6bec69
Elinsz <85796934+Elinsz@users.noreply.github.com>
2025-02-15 02:41:12 -0300

    cm

--- assets/js/script_ed-3d.js
+++ assets/js/script_ed-3d.js
@@ -73,10 +73,10 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // Ajusta a tela em caso de redimensionamento
-    window.addEventListener('resize', () => {
-    camera.aspect = (window.innerWidth - 250) / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth - 250, window.innerHeight);
+    // window.addEventListener('resize', () => {
+    // camera.aspect = (window.innerWidth - 250) / window.innerHeight;
+    // camera.updateProjectionMatrix();
+    // renderer.setSize(window.innerWidth - 250, window.innerHeight);
 
 //============================================================
 
