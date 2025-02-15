72d66d98a89bc4fc63e81261328e8c5a85c7fd3a
Elinsz <85796934+Elinsz@users.noreply.github.com>
2025-02-15 16:29:29 -0300

    cm

--- assets/js/script_ed-3d.js
+++ assets/js/script_ed-3d.js
@@ -182,14 +182,11 @@ function applyDimensions() {
 
         if (piece) {
             driverBlock.add(piece);
-            enableDrag(piece);
-            // piece.position.set(0, 0, 0); // Posição inicial dentro do Driver-Block
-            piece.position.set(20, 0, 0); // Exemplo de movimentação manual
-
-
-            // Aqui você pode ajustar a posição da peça manualmente depois
+            enableDrag(piece); // Habilita arrastar com o mouse
             console.log(`${pieceType} adicionada dentro do Driver-Block.`);
         }
+
+
     });
 
 
