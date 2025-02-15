cc89932585dbc5419dea9e9f8b70550477301488
Elinsz <85796934+Elinsz@users.noreply.github.com>
2025-02-15 14:07:16 -0300

    cm

--- assets/js/script_ed-3d.js
+++ assets/js/script_ed-3d.js
@@ -128,3 +128,18 @@ document.addEventListener("DOMContentLoaded", () => {
     window.loadModules = loadModules;
 
 });
+
+    // CONTROLE DOS DROPDOWNS
+    document.querySelectorAll('.dropdown-btn').forEach(button => {
+        button.addEventListener('click', function () {
+            const menu = this.parentElement;
+            menu.classList.toggle('active');
+        });
+    });
+
+    // Ajusta a tela em caso de redimensionamento
+        window.addEventListener('resize', () => {
+        camera.aspect = (window.innerWidth - 250) / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth - 250, window.innerHeight);
+    });
