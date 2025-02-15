ef018c011209e7c4f96edc9a331f45603906ba23
Elinsz <85796934+Elinsz@users.noreply.github.com>
2025-02-15 17:23:58 -0300

    cm

--- assets/js/script_ed-3d.js
+++ assets/js/script_ed-3d.js
@@ -121,7 +121,7 @@ function applyDimensions() {
         );
 
         // Desloca a geometria para que o canto inferior esquerdo seja (0, 0, 0)
-        geometry.translate(0, 0, profundidade / 2);
+        geometry.translate(altura / 2, 0,0);
 
         const material = new THREE.MeshBasicMaterial({
             color: cor,
