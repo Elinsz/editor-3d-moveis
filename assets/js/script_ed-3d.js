dc4d1016acdb377ae7149cbe8960e36bffd17cc3
Elinsz <85796934+Elinsz@users.noreply.github.com>
2025-02-17 20:30:59 -0300

    cm

--- assets/js/script_ed-3d.js
+++ assets/js/script_ed-3d.js
@@ -33,8 +33,8 @@ function addEnvironment() {
         const gridHelper = new THREE.GridHelper(10000, 500, 0x888888, 0x444444);
         gridHelper.material.opacity = 0.5;
         gridHelper.material.transparent = true;
-        // gridHelper.rotation.z = Math.PI; // Rotaciona 180 graus em torno do eixo Z
-        gridHelper.rotation.x = Math.PI; // Rotaciona 180 graus em torno do eixo X
+        gridHelper.rotation.z = Math.PI; // Rotaciona 180 graus em torno do eixo Z
+        // gridHelper.rotation.x = Math.PI; // Rotaciona 180 graus em torno do eixo X
         scene.add(gridHelper);
 
         // Eixos X, Y, Z
