b1ba1b341cc9961685b89c45f6a2f47853f7c3df
Elinsz <85796934+Elinsz@users.noreply.github.com>
2025-02-17 21:07:08 -0300

    cm

--- assets/js/script_ed-3d.js
+++ assets/js/script_ed-3d.js
@@ -116,8 +116,8 @@ function addModule() {
         driverBlock.material.dispose();
     }
 
-    const width = parseFloat(document.getElementById('width').value) || 500;
-    const height = parseFloat(document.getElementById('height').value) || 800;
+    const width = parseFloat(document.getElementById('width').value) || 300;
+    const height = parseFloat(document.getElementById('height').value) || 750;
     const depth = parseFloat(document.getElementById('depth').value) || 500;
 
     const geometry = new THREE.BoxGeometry(width, height, depth);
@@ -129,7 +129,7 @@ function addModule() {
     });
 
     driverBlock = new THREE.Mesh(geometry, material);
-    driverBlock.position.set(height / 2 , depth/ 2, width  / 2);
+    driverBlock.position.set(width / 2 , height / 2, depth / 2);
     scene.add(driverBlock);
 }
 
