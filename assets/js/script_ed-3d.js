9809427278938b2366d01aa76f5938fc64055fbf
Elinsz <85796934+Elinsz@users.noreply.github.com>
2025-02-17 23:09:44 -0300

    cm

--- assets/js/script_ed-3d.js
+++ assets/js/script_ed-3d.js
@@ -127,9 +127,9 @@ function addModule() {
     const material = new THREE.MeshBasicMaterial({
         color: 0x00ffff,
         transparent: true,
-        opacity: 0.5,
-        Realistic: true
-        // wireframe: false
+        opacity: 0.9,
+
+        wireframe: true
 
     });
 
