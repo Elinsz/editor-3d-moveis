9148820fa66e6ccf4381e1bc6a9cef788f96882e
Elinsz <85796934+Elinsz@users.noreply.github.com>
2025-02-17 21:00:25 -0300

    cm

--- assets/js/script_ed-3d.js
+++ assets/js/script_ed-3d.js
@@ -71,8 +71,8 @@ function addEnvironment() {
     const axisLength = 300;
 
     const xAxis = new THREE.ArrowHelper(
-        new THREE.Vector3(0, 1, 0),
         new THREE.Vector3(0, 0, 0),
+        new THREE.Vector3(0, 0, 1),
         axisLength,
         0xff0000,
         20,
