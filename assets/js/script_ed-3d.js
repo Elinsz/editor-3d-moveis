6f21468495072c7127612e2b95c220d30b286e87
Elinsz <85796934+Elinsz@users.noreply.github.com>
2025-02-17 22:33:15 -0300

    cm

--- assets/js/script_ed-3d.js
+++ assets/js/script_ed-3d.js
@@ -296,7 +296,7 @@ function createBase(largura, profundidade, espessura, cor = 0x00ff00) {
         if (piece) {
             driverBlock.add(piece);
             // piece.position.set(0, 0, 0); // Posição inicial dentro do Driver-Block
-            piece.position.set(height/2, 0, 0); // Exemplo de movimentação manual
+            piece.position.set(20, 0, 0); // Exemplo de movimentação manual
 
             // Aqui você pode ajustar a posição da peça manualmente depois
             console.log(`${pieceType} adicionada dentro do Driver-Block.`);
