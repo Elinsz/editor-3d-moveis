51da3469a55607c020b8efa05268fd653453d15f
Elinsz <85796934+Elinsz@users.noreply.github.com>
2025-02-17 23:22:15 -0300

    cm

--- assets/js/script_ed-3d.js
+++ assets/js/script_ed-3d.js
@@ -125,7 +125,7 @@ function addModule() {
 
     const geometry = new THREE.BoxGeometry(width , height, depth);
     const material = new THREE.MeshBasicMaterial({
-        color: 0x000ff,
+        color: 0x0f0f0,
         transparent: true,
         opacity: 0.2,
         // wireframe: false
