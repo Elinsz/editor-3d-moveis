6bf002684303f3f3eaf53eca0bdf1689544cf452
Elinsz <85796934+Elinsz@users.noreply.github.com>
2025-02-17 23:26:09 -0300

    cm

--- assets/js/script_ed-3d.js
+++ assets/js/script_ed-3d.js
@@ -123,7 +123,7 @@ function addModule() {
     const height = parseFloat(document.getElementById('height').value) || 750;
     const depth = parseFloat(document.getElementById('width').value) || 500;
 
-    const geometry = new THREE.BoxGeometry(width , height, depth, cor = 0x0fff0);
+    const geometry = new THREE.BoxGeometry(width , height, depth, cor = 0x00ff00);
     const material = new THREE.MeshBasicMaterial({
         color: cor,
         transparent: true,
